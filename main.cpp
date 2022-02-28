585c5dced958a74e6458227191c3ee269c44ccaf
Adoche_Onaji <94250578+Dochie@users.noreply.github.com>
2022-02-28 20:02:25 +0100

    Add files via upload
    Assignment 2

--- main.cpp
+++ main.cpp
@@ -4,19 +4,27 @@ using namespace std;
 
 int main()
 {
-/* The common field cricket chirps in direct proportion to the current temperature. Adding 40 to
-the number of times a cricket chirps in a minute, then dividing that value by 4 gives us the
-temperature. Write a program that takes as input the number of chirps in a minute and prints
-the current temperature.*/
+    float a1,a2,a3,a4,m,s,f,mc,sc,fc,ac,final_grade;
+    cout<<"Enter the value for your first assignment";
+    cin>>a1;
+    cout<<"Enter the value for your second assignment";
+    cin>>a2;
+    cout<<"Enter the value for your third assignment";
+    cin>>a3;
+    cout<<"Enter the value for your forth assignment";
+    cin>>a4;
+    cout<<"Enter the value for your midterm examination";
+    cin>>m;
+    cout<<"Enter the value for your final examination";
+    cin>>f;
+    cout<<"Enter the value for your section examination";
+    cin>>s;
 
-//c proportional to t
-
-float c;
-cout<< "Input the value of times the cricket chirps: ";
-cin>> c;
-float t;
-t = (c + 40)/4;
-cout<< "The temperature is: ";
-cout<< t;
-return 0;
+    mc=m*0.15;
+    fc=f*0.35;
+    sc=s*0.1;
+    ac=((a1+a2+a3+a4)/4*0.4);
+    final_grade=ac+mc+fc+sc;
+    cout<<final_grade;
+    return 0;
 }
